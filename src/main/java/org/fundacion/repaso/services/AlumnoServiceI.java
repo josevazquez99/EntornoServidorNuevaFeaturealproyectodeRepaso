752341feb02aa641e319903103b92c492a3c4894
José Antonio Vázquez <javazquez@uc-3-337.AluVedruna.local>
2024-10-29 15:17:27 +0100

    terminado

---
+++ src/main/java/org/fundacion/repaso/services/AlumnoServiceI.java
@@ -0,0 +1,15 @@
+package org.fundacion.repaso.services;
+
+import java.sql.Date;
+import java.util.List;
+
+import org.fundacion.repaso.dto.AlumnoDTO;
+import org.fundacion.repaso.dto.CrearAlumnoDTO;
+import org.fundacion.repaso.dto.NotaDTO;
+
+public interface AlumnoServiceI {
+    AlumnoDTO getAlumnoByEmail(String email);
+    List<AlumnoDTO> getAlumnosBetweenBirthdates(Date from_date, Date to_date);
+    void insertAlumno(CrearAlumnoDTO alumno);
+
+}
